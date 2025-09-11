9088fb40a77a90ffef911f210538c96df10bc8cf
GKDevotion <gk@devotiontech.io>
2025-09-11 15:44:05 +0530

    add carousel slider

--- index.php
+++ index.php
@@ -39,6 +39,34 @@ if( false ){
         </div>
     </header>
     <?php
+} else if( true ){
+    ?>
+    <div id="indexCarousel" class="carousel slide" data-bs-ride="carousel">
+        <!-- Indicators -->
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#indexCarousel" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#indexCarousel" data-bs-slide-to="1"></button>
+        </div>
+
+        <!-- Slides -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="assets/images/slider/Navigate-Market.png" class="d-block w-100" alt="Slide 1">
+            </div>
+            <div class="carousel-item">
+                <img src="assets/images/slider/Navigate-Market-ZCapital.png" class="d-block w-100" alt="Slide 2">
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#indexCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#indexCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+    </div>
+    <?php
 } else {
     ?>
     <header class="header-banner position-relative d-flex justify-content-center align-items-center text-center">
