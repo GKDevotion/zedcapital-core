4fa4aee4b0ec1eb8d81138802f348253bffdc09b
GKDevotion <gk@devotiontech.io>
2025-10-15 15:40:25 +0530

    update product forex trade slider

--- forex.php
+++ forex.php
@@ -210,7 +210,7 @@ include_once ('elements/header.php');
         padding: 20px;
         text-align: center;
         background-color: aliceblue;
-        margin: 10px 25px;
+        margin: 10px 0px;
     }
     .btn-buy {
         background-color: var(--zed-success);
